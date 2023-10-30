5d746d226e39f47d93b9ee1fa5fc7a568d42c253
iiihassanii <hassanabdelazim.max@gmail.com>
2023-10-30 05:54:53 -0700

    hey

---
+++ 10-check_cycle.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * check_cycle - function checks if a singly linked list has a cycle in it.
+ * @list: pointer to the beginning of the node
+ * Return: 0 if no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *node, *check;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+	node = list;
+	check = node->next;
+
+	while(check != NULL)
+	{
+		if (node == check)
+			return (1);
+		node = list;
+		check = node->next;
+	}
+	return (0);
+}
