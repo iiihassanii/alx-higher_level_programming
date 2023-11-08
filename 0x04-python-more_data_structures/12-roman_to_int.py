08d0c82816a045ef27cf918381472d48fa9a98df
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-08 16:46:47 +0300

    Hmm

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(roman_string):
 
     result = 0
     prev_value = 0
-    roman = [I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000]
+    roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     for ch in reversed(roman_string):
         value = roman[ch]
         
