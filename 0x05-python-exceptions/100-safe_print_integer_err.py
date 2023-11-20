25bb3e432a877285538af1f103dddb8e5c13beda
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-20 15:12:14 +0300

    fedf

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError:
-        print("Exception: Value is not an integer", file=sys.stderr)
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return False
-
