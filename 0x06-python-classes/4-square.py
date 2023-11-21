c972cb1debdaec8947df40e9124327b011cffbe1
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-21 14:48:05 +0300

    way

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -10,7 +10,8 @@ class Square:
         Initializes a new instance of the Square class.
 
         Args:
-            size (int, optional): The length of the square's sides. Defaults to 0.
+            size (int, optional): The length of the square's sides.
+            Defaults to 0.
 
         Raises:
             TypeError: If size is not an integer.
