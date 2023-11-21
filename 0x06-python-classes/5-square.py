687f9826bc75e4b19ed9d1e30faa36dde4d97233
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-21 15:11:35 +0300

    waay

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""square """
+
+
+class Square:
+    """Represents a square with a given size."""
+
+    def __init__(self, size=0):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            size (int, optional): The length of the square's sides.
+            Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.__size = 0  # Initialize with default value
+        self.size = size  # Use the setter method to validate and set the size
+
+    @property
+    def size(self):
+        """
+        Retrieves the size of the square.
+
+        Returns:
+            int: The length of the square's sides.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Args:
+            value (int): The new length of the square's sides.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Prints a square of '#' characters.
+
+        Each side of the square has a length equal to the size of the square.
+
+        Returns:
+            None
+        """
+        for i in range(self.__size):
+            print("#" * self.__size)
