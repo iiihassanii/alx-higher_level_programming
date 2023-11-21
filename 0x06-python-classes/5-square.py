3c2251aad145c4c4db60ffa967965aea17a81db5
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-21 15:15:23 +0300

    waay

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -56,7 +56,7 @@ class Square:
             int: The area of the square.
         """
         return self.__size * self.__size
-    
+
     def my_print(self):
         """
         Prints a square of '#' characters.
