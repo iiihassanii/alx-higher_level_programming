e73acef4f165ddbd6c4dd86f9e726cdbc5bb962a
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-28 13:25:24 +0300

    hmm

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-"""_summary_"""
-
 
 def copy_list(lst):
-    """_summary_
-
-    Args:
-        l (_type_): _description_
-
-    Returns:
-        _type_: _description_
-    """
     rlist = []
     for i in lst:
         rlist.append(i)
