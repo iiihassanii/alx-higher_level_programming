3c25787fdba59e4a5db47ebb7fe5b25850bfe94d
iiihassanii <hassanabdelazim.max@gmail.com>
2023-11-28 13:17:14 +0300

    20 ?

---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""_summary_
+    """
+
+
+def copy_list(l):
+    """_summary_
+
+    Args:
+        l (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    rlist = []
+    for i in l:
+        rlist.append(i)
+    return rlist
