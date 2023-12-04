1ab93954b820432a7222fe12ade41fabbbd0b481
iiihassanii <hassanabdelazim.max@gmail.com>
2023-12-04 15:42:45 +0300

    hmm

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """_summary_
-	"""
+"""
 
 
 def lookup(obj):
     """_summary_
 
-    Args:
-            obj (_type_): _description_
+Args:
+    obj (_type_): _description_
 
-    Returns:
-            list: available attributes and methods of an object:
-    """
+Returns:
+    list: available attributes and methods of an object:
+"""
     return dir(obj)
