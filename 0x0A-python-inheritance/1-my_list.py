c907e6743e1961cc88fcd817abc47a7b8438f10e
iiihassanii <hassanabdelazim.max@gmail.com>
2023-12-04 16:01:58 +0300

    Hmm

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""_summary_"""
+
+
+class MyList(list):
+    """_summary_
+
+    Args:
+            list (_type_): _description_
+    """
+
+    def print_sorted(self):
+        """print sorted list"""
+        print(sorted(self))
