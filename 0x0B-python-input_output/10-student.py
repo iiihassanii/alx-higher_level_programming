a82a955d6c95d1df019e27ebdcd771d14df3d5c9
iiihassanii <hassanabdelazim.max@gmail.com>
2023-12-05 17:24:32 +0300

    hmm

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""class Student"""
+
+
+class Student:
+    """class Student"""
+
+    def __init__(self, first_name, last_name, age):
+        """_summary_
+
+        Args:
+            first_name (_type_): _description_
+            last_name (_type_): _description_
+            age (_type_): _description_
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for attr in attrs if hasattr(self, attr)}
