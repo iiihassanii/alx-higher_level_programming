55e0d6edc5d2b6f6b298ff4da9d7d6308e90c68d
iiihassanii <hassanabdelazim.max@gmail.com>
2023-12-05 15:42:59 +0300

    HII

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" READ FILE"""
+
+
+def read_file(filename=""):
+    """read data from file"""
+    with open(filename, 'r', encoding="utf-8") as f:
+
+        read_data = f.read()
+        print(read_data)
+    f.closed
