74d13f0b282b0295d1dd19775c585ed584029d8f
iiihassanii <hassanabdelazim.max@gmail.com>
2023-12-11 18:35:06 +0300

    att edit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,60 +14,6 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-    def update(self, *args, **kwargs):
-        """_summary_
-        """
-        # if args is None:
-        #   self.__init__(self.width, self.height, self.x, self.y)
-        if args:
-            if len(args) > 0:
-                self.id = args[0]
-            if len(args) > 1:
-                self.width = args[1]
-            if len(args) > 2:
-                self.height = args[2]
-            if len(args) > 3:
-                self.x = args[3]
-            if len(args) > 4:
-                self.y = args[4]
-        elif kwargs:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """Return the dictionary representation of a Rectangle."""
-        return {
-            "id": self.id,
-            "width": self.width,
-            "height": self.height,
-            "x": self.x,
-            "y": self.y
-        }
-
-    def display(self):
-        """print '#' n times
-        """
-        print("\n" * self.y, end="")
-        for row in range(self.__height):
-            print(" " * self.x, end="")
-            for col in range(self.__width):
-                print("#", end="")
-            print()
-
-    def area(self):
-        """calc Areavi """
-        return self.__width * self.__height
-
-    def __str__(self) -> str:
-        """_summary_
-
-        Returns:
-            str: _description_
-        """
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
-                                                       self.x, self.y,
-                                                       self.width, self.height)
-
     @property
     def width(self):
         """width"""
@@ -123,3 +69,57 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be > 0")
         self.__y = value
+
+    def update(self, *args, **kwargs):
+        """_summary_
+        """
+        # if args is None:
+        #   self.__init__(self.width, self.height, self.x, self.y)
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def display(self):
+        """print '#' n times
+        """
+        print("\n" * self.y, end="")
+        for row in range(self.__height):
+            print(" " * self.x, end="")
+            for col in range(self.__width):
+                print("#", end="")
+            print()
+
+    def area(self):
+        """calc Areavi """
+        return self.__width * self.__height
+
+    def __str__(self) -> str:
+        """_summary_
+
+        Returns:
+            str: _description_
+        """
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
