edd9a1e7d55e42412b6f672f55c229b450c5af1f
iiihassanii <hassanabdelazim.max@gmail.com>
2023-12-11 19:06:44 +0300

    sqr edit

---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""_summary_
+        """
+
+import unittest
+from models.base import Base
+import pytest
+import json
+from models.base import Base
+from models.square import Square
+
+
+class TestBase(unittest.TestCase):
+    pass
