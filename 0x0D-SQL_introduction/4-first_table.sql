81b92792c3988553d61f0c05bfee757df25c82f4
iiihassanii <hassanabdelazim.max@gmail.com>
2024-01-16 15:15:44 +0300

    SQL

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- creates a table called first_table.
-CREATE TABLE `first_table` IF `first_table` NOT EXISTS 
+IF `first_table` NOT EXISTS CREATE TABLE `first_table`
 {
         `id` (INT),
         `name` VARCHAR(256)
