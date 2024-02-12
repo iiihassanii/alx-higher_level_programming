26afaf2b1678f0963d44099a92914579761da37c
iiihassanii <hassanabdelazim.max@gmail.com>
2024-02-12 16:58:49 +0300

    task8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (isNaN(process.argv[2])) { console.log('Missing size'); } else {
+  for (let i = 0; i < process.argv[2]; i++) {
+    let row = '';
+    for (let j = 0; j < process.argv[2]; j++) { row += 'X'; }
+    console.log(row);
+  }
+}
