5456e4d687a49d05506a39e96af358e7a20f9b02
iiihassanii <hassanabdelazim.max@gmail.com>
2024-02-12 18:55:31 +0300

    tasl 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,16 +1,9 @@
 #!/usr/bin/node
-const len = process.argv.length;
-let max, min;
-if (len <= 3) {
+const args = process.argv.slice(2).map(Number);
+
+if (args.length <= 1) {
   console.log(0);
 } else {
-  min = 2;
-  max = 2;
-  for (let i = 2; i < len; i++) {
-    if (Number(process.argv[i]) > Number(process.argv[max])) {
-      min = max;
-      max = i;
-    } else if (Number(process.argv[i]) < process.argv[min]) { min = i; }
-  }
-  console.log(process.argv[min]);
+  args.sort((a, b) => b - a);
+  console.log(args[1]);
 }
