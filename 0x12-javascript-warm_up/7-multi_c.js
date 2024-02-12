a97b31ccecbf5ab45884ab0ccef5b02bfe3a8afd
iiihassanii <hassanabdelazim.max@gmail.com>
2024-02-12 16:38:22 +0300

    task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+if (!isNaN(process.argv[2]) && process.argv[2] > 0) {
+  for (let i = 0; i < process.argv[2]; i++) { console.log('C is fun'); }
+} else { console.log('Missing number of occurrences'); }
