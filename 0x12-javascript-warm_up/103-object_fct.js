1a47aa658f9105223fc6eb7e1607584b0f965f6e
iiihassanii <hassanabdelazim.max@gmail.com>
2024-02-12 19:23:26 +0300

    task 102

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function (){
-  myObject.value +=1;
-}
+myObject.incr = function () {
+  myObject.value += 1;
+};
 /*
 YOUR CODE HERE
 */
