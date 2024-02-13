4fb562a2152893e76d0fef5c457e5df2df07269f
iiihassanii <hassanabdelazim.max@gmail.com>
2024-02-13 15:35:44 +0300

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) { c = 'X'; }
+    for (let i = 0; i < this.width; i++) {
+      let line = '';
+      for (let j = 0; j < this.height; j++) { line += c; }
+      console.log(line);
+    }
+  }
+}
+module.exports = Square;
