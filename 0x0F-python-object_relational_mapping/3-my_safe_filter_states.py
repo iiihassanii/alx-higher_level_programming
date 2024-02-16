7ec88bd87c79b165c5bb8548c08dcd090945cd26
iiihassanii <hassanabdelazim.max@gmail.com>
2024-02-16 13:16:16 +0300

    tasks

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-        f"SELECT * FROM states WHERE name LIKE BINARY %s", (sys.argv[4], ))
+        "SELECT * FROM states WHERE name LIKE BINARY %s", (sys.argv[4], ))
     result = cursor.fetchall()
     for row in result:
         print(row)
