6324535c1651418108930fe6cf6f48b16e4e0384
iiihassanii <hassanabdelazim.max@gmail.com>
2024-02-17 15:06:31 +0300

    task 10

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -11,12 +11,16 @@ if __name__ == "__main__":
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
+
     Session = sessionmaker(bind=engine)
 
     session = Session()
 
-    state = session.query(State).filter(State.name == (sys.argv[4],))
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+
     if state:
-        print(state[0].id)
+        print(state.id)
     else:
-        print("Not founds")
+        print("Not found")
+
+    session.close()
