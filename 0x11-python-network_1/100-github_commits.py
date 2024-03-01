131cfa537544a9cc1037dcb352af5804885a26d3
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-01 19:23:12 +0300

    task 100

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Python script that takes 2 arguments in
+order to solve this challenge."""
+import requests
+import sys
+# /repos/{owner}/{repo}/commits
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    try:
+        r = requests.get(url)
+        r.raise_for_status()
+        commits = r.json()
+
+    except requests.RequestException as e:
+        pass
+
+    for i in range(10):
+        print("{}: {}".format(
+            commits[i].get("sha"),
+            commits[i].get("commit").get("author").get("name")))
