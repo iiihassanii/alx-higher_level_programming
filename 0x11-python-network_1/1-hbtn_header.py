a1d783c15efea17985788b75ca8759914e9697a9
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-01 14:17:29 +0300

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@ import urllib.request
 import sys
 """Python script that takes
 in a URL, sends a request to the URL"""
-with urllib.request.urlopen(sys.argv[1]) as response:
-    item = response.headers.get('X-Request-Id')
-    print(item)
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        item = response.headers.get('X-Request-Id')
+        print(item)
