4e5546413d0bed2b587ac2d1d68e0493a8ada864
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-01 16:00:26 +0300

    task 7

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""Python script that takes in a URL, sends a request to
-the URL and displays the value of the variable
-X-Request-Id in the response header"""
+"""Write a Python script that takes in a URL and an
+email address, sends a POST request to the passed
+URL with the email as a parameter, and finally displays
+the body of the response"""
 import requests
 import sys
 if __name__ == "__main__":
