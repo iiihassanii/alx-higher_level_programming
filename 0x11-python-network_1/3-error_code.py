3668a6cdbc9317178c54c995c06f26ba42671da3
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-01 15:24:01 +0300

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,11 @@
 """Python script that takes in a URL,
 sends a request to the URL and displays
 the body of the response (decoded in utf-8)."""
-from urllib import request, parse, error
+from urllib import request, error
 import sys
 url = sys.argv[1]
 try:
-    response = request.urlopen(url)
-    print(response.read().decode('utf-8'))
+    with request.urlopen(sys.argv[1]) as response:
+        print(response.read().decode('UTF-8'))
 except error.URLError as e:
     print("Error code: ", e.code)
