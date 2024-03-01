8e4af2a2c3ec93c2f35facbaa4b781c362bc62ac
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-01 14:12:11 +0300

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -5,8 +5,10 @@ import sys
 in a URL, sends a request to the URL
 and displays the value of the X-Request-Id
 variable found in the header of the response."""
-url = sys.argv[1]
 
+
+url = sys.argv[1]
 with request.urlopen(url) as response:
-    if 'X-Request-Id' in response.headers:
-        print(response.headers['X-Request-Id'])
+    item = response.headers.get('X-Request-Id')
+    if item:
+        print(item)
