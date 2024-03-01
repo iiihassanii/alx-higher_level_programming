442dfc289c5dc39546195470c3d59c4734ceb3d0
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-01 16:58:50 +0300

    task 8

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,8 +8,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    status = response.status_code
-    if status >= 400:
+    if response.status_code >= 400:
         print("Error code: {}".format(response.status_code))
     else:
         print(response.text)
