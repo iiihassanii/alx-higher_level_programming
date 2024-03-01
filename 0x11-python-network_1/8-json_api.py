d42a625b40112904c1968b8d6bf2b41ebc978e0a
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-01 18:24:27 +0300

    task 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -16,10 +16,10 @@ if __name__ == "__main__":
             'http://0.0.0.0:5000/search_user', data=values)
         json_response = response.json()
 
-        if response == {}:
-            print("No result")
-        else:
+        if "id" in json_response and "name" in json_response:
             print("[{}] {}".format(json_response.get(
                 "id"), json_response.get("name")))
+        else:
+            print("No result")
     except ValueError:
         print("Not a valid JSON")
