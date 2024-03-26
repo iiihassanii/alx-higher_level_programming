6dede96be28041c7d9b45b602bafd16518615e50
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-26 15:00:12 +0300

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,10 @@ const fs = require('fs');
 const fileName = process.argv[2];
 const string = process.argv[3];
 
-fs.open(fileName, 'a', function (fd) {
-  fs.write(fd, string, function () {
-  });
+fs.open(fileName, 'a', function (err, fd) {
+  if (err) { console.log(err); } else {
+    fs.write(fd, string, 'utf8', function (err) {
+      if (err) { console.log(err); }
+    });
+  }
 });
