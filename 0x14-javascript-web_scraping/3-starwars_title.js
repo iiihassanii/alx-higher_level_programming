19e87507aa38c084330ccde0743ef06623a34b4f
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-26 15:38:11 +0300

    task 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -5,6 +5,6 @@ const url = 'https://swapi-api.alx-tools.com/api/films/';
 request(url + episode, function (error, response, body) {
   if (!error) {
     const movie = JSON.parse(body);
-    console.log('Title:', movie.title);
+    console.log(movie.title);
   }
 });
