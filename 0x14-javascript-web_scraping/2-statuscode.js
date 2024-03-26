c3c3d7d3df79f04beff5b174fed350ddc11f7167
iiihassanii <hassanabdelazim.max@gmail.com>
2024-03-26 15:21:41 +0300

    task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,10 +4,10 @@ const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) {
-    console.log('Status Code:', response.statusCode);
+    console.log('code:', response.statusCode);
     return;
   }
   if (response.statusCode) {
-    console.log('Status Code:', response.statusCode);
+    console.log('code:', response.statusCode);
   }
 });
